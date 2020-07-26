0512a67f3922f708be45d94339f09d3fb7a0348d
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-07-27 05:09:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def predict():
 	X = cv.fit_transform(X) # Fit the Data
 	from sklearn.model_selection import train_test_split
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-	from sklearn.naive_bayes import MultinomialNB
-	clf = MultinomialNB(alpha=0.2)
+	##	from sklearn.naive_bayes import MultinomialNB
+	clf = LogisticRegression()
 	clf.fit(X_train,y_train)
 	clf.score(X_test,y_test)
 	xp=""
